9c6776c4427d78138a14a0a46d45ae23122b9c29
unknown <tatlidilonur0@gmail.com>
2024-06-12 19:39:56 -0400

    all changes pushed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Header from './features/header/Header'
+import TodoList from './features/todos/TodoList'
+import Footer from './features/footer/Footer'
 
 function App() {
   return (
@@ -6,12 +9,21 @@ function App() {
       <nav>
         <section>
           <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
         </section>
       </nav>
+
+      <main>
+        <section className="medium-container">
+          <h2>Todos</h2>
+          <div className="todoapp">
+            <Header />
+            <TodoList />
+            <Footer />
+          </div>
+        </section>
+      </main>
+
+      
       <section>
         <h2>Welcome to the Redux Fundamentals example app!</h2>
       </section>
